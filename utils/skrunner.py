e5e751b2c46e412c99763e01acc7ee8fc74c9ef9
Paul Maddern <paul@arcadegeek.co.uk>
2022-07-05 15:48:16 +0000

    Updates 05072022.

--- utils/skrunner.py
+++ utils/skrunner.py
@@ -6,6 +6,7 @@ HOBBIT_HOME = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 HOBBIT_SKOOL = '{}/sources/hobbit.skool'.format(HOBBIT_HOME)
 
 SKOOLKIT_HOME = os.environ.get('SKOOLKIT_HOME')
+SKOOLKIT_TOOLS = "{}/tools".format(SKOOLKIT_HOME)
 if SKOOLKIT_HOME:
     if not os.path.isdir(SKOOLKIT_HOME):
         sys.stderr.write('SKOOLKIT_HOME={}: directory not found\n'.format(SKOOLKIT_HOME))
@@ -25,9 +26,9 @@ def run_skool2asm():
     skool2asm.main(sys.argv[1:] + [HOBBIT_SKOOL])
 
 def run_skool2html():
-    options = '-c Config/InitModule=sources:bases -d {}/build/html'.format(HOBBIT_HOME)
+    options = "-c Config/InitModule={}:publish -d {}/build/html".format(SKOOLKIT_TOOLS, HOBBIT_HOME)
     art.tprint("The Hobbit")
-    hex = '-H -c Config/GameDir=hobbit/hex'
-    dec = '-D -c Config/GameDir=hobbit/dec'
+    hex = '-H -c Config/GameDir=hobbit --var pub=2'
+    dec = '-D -c Config/GameDir=hobbit/dec --var pub=4'
     skool2html.main(options.split() + hex.split() + sys.argv[1:] + [HOBBIT_SKOOL])
     skool2html.main(options.split() + dec.split() + sys.argv[1:] + [HOBBIT_SKOOL])
